da997066f756b882dfc156045cd4c2e6f8132f22
ThomasBucaioni <thomas.bucaioni@gmail.com>
2021-04-14 05:56:12 +0200

    increase length

--- Shell/Passwords/sAddPassword.sh
+++ Shell/Passwords/sAddPassword.sh
@@ -27,7 +27,7 @@ fi
 symbolsArray=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 % $ \# @ ! \&)
 
 #password string accumulator
-passwordString="aA."
+passwordString="aA0."
 
 #site
 if [ -z $1 ]; then
@@ -47,14 +47,14 @@ else
 fi
     #length of the password
 if [ -z $3 ]; then
-    echo 'Default password length is 16'
+    echo 'Default password length is 20'
     #exit
-    length=13
-elif [[ $1 -ge 3 ]]; then
+    length=16
+elif [[ $1 -ge 4 ]]; then
     echo "Password of length $1"
-    length=$(($1 - 3))
+    length=$(($1 - 4))
 else
-    echo 'Password of length 3'
+    echo 'Password of length 4'
     length=0
 fi
 
