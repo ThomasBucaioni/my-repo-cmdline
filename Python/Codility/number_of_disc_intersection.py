54142b4d1a2484c6f262e0270ca6d06224b51f31
Thomas Bucaioni <thomas.bucaioni@gmail.com>
2021-12-23 14:11:02 +0100

    Number of disc intersection ok but 88% timeout

--- Python/Codility/number_of_disc_intersection.py
+++ Python/Codility/number_of_disc_intersection.py
@@ -128,7 +128,7 @@ def solution(A):
 
     return nbdrectot+nbsrectot
 
-def solution(A):
+def solutionref(A):
 
     l = len(A)
 
@@ -150,14 +150,148 @@ def solution(A):
                 n +=1
             #else:
             #    break
-    print(f"{n=}")
+    print(f"Sol ref: {n=}")
     return n
 
-A = [1,5,2,1,4,0]
-print(11 == solution(A), '\n')
-A = [1,1,1]
-print(3 == solution(A), '\n')
-A = [1, 10, 100, 1]
-print(5 == solution(A), '\n')
-A = [1, 0, 1, 0, 1]
-print(6 == solution(A), '\n')
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.disable(logging.DEBUG)
+
+def solution(A):
+
+    l = len(A)
+
+    T = []
+    for i in range(len(A)):
+        T.append((i,A[i]))
+
+    B = sorted(T, key=lambda tup: (tup[1],tup[0]))
+
+    B.reverse()
+
+    logging.debug('----------')
+    logging.debug(f"{A}")
+    logging.debug('----------')
+    logging.debug(f"{B}")
+    
+    n = 0
+    
+    for i in range(l):
+        logging.debug('----------')
+        p = B[i][0]
+        r = B[i][1]
+        logging.debug(f"{p=}, {r=}, M={min(p+r+1, l)}, m={max(p-r-1, 0)}")
+        for j in range(p+1, min(p+r+1, l)):
+            logging.debug(f"\t{j=}", end='')
+            if j - p > A[j]:
+                n += 1
+                logging.debug(f", overlap: {n=}")
+            elif A[j] < A[p]:
+                n += 1
+                logging.debug(f", overlap: {n=}")
+            elif A[j] == A[p] and p > j:
+                n += 1
+                logging.debug(f", overlap: {n=}")
+            else:
+                logging.debug('')
+        for j in range(max(p-r, 0), p):
+            logging.debug(f"\t{j=}", end='')
+            if p - j > A[j]:
+                n += 1
+                logging.debug(f", overlap: {n=}")
+            elif A[j] < A[p]:
+                n += 1
+                logging.debug(f", overlap: {n=}")
+            elif A[j] == A[p] and p > j:
+                n += 1
+                logging.debug(f", overlap: {n=}")
+            else:
+                logging.debug('')
+        if i < l-1:
+            s = B[i+1][1]
+            logging.debug(f"pp={B[i+1][0]},rp={s}")
+            for j in range(p+r+1, min(p+r+s+1, l)):
+                logging.debug(f"\t{j=}")
+                if j - p <= r + A[j] and (A[j] < r or (A[j] == r and p > j)):
+                    n += 1
+                    logging.debug(f"\t\toverlap: {n=}")
+            for j in range(max(p-r-s-1, 0), p-r):
+                logging.debug(f"\t{j=}")
+                if p - j <= r + A[j] and (A[j] < r or (A[j] == r and p > j)):
+                    n += 1
+                    logging.debug(f"\t\toverlap: {n=}")
+        logging.debug(f"{n=}")
+        #input("Press Enter to continue...")
+    logging.debug(f"{n=}")
+    #input("Press Enter to continue...")
+    return n
+
+#if (False):
+if (True):
+    A = [1, 2, 0, 1, 1, 2]
+    print('>>>>>> ', solutionref(A) == solution(A), ' <<<<<<\n')
+    A = [1,5,2,1,4,0]
+    print('>>>>>> ', solutionref(A) == solution(A), ' <<<<<<\n')
+    A = [1,1,1]
+    print('>>>>>> ', solutionref(A) == solution(A), ' <<<<<<\n')
+    A = [1, 10, 100, 1]
+    print('>>>>>> ', solutionref(A) == solution(A), ' <<<<<<\n')
+    A = [1, 0, 1, 0, 1]
+    print('>>>>>> ', solutionref(A) == solution(A), ' <<<<<<\n')
+
+import random
+N = 20
+A = [0] * N
+for i in range(N):
+    for j in range(N):
+        A[j] = random.randint(0,N)
+    print('>>>>>> ', solutionref(A) == solution(A), ' <<<<<<\n')
+    input("Press Enter to continue...")
+    
+
+
+
+def solutionOkButTimeOut(A):
+
+    l = len(A)
+
+    T = []
+    for i in range(len(A)):
+        T.append((i,A[i]))
+
+    B = sorted(T, key=lambda tup: (tup[1],tup[0]))
+
+    B.reverse()
+
+    n = 0
+    
+    for i in range(l):
+        p = B[i][0]
+        r = B[i][1]
+        for j in range(p+1, min(p+r+1, l)):
+            if j - p > A[j]:
+                n += 1
+            elif A[j] < A[p]:
+                n += 1
+            elif A[j] == A[p] and p > j:
+                n += 1
+
+        for j in range(max(p-r, 0), p):
+            if p - j > A[j]:
+                n += 1
+            elif A[j] < A[p]:
+                n += 1
+            elif A[j] == A[p] and p > j:
+                n += 1
+
+        if i < l-1:
+            s = B[i+1][1]
+            for j in range(p+r+1, min(p+r+s+1, l)):
+                if j - p <= r + A[j] and (A[j] < r or (A[j] == r and p > j)):
+                    n += 1
+            for j in range(max(p-r-s-1, 0), p-r):
+                if p - j <= r + A[j] and (A[j] < r or (A[j] == r and p > j)):
+                    n += 1
+    return n
+
